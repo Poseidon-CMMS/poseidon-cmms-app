a9913becd84c0f413a25cfbd8b7d2bf099a01652
Mauricio Lima <limamauricio303@gmail.com>
2021-12-11 01:22:56 -0300

    work in progress assignation update

--- src/components/AssignationDialog.vue
+++ src/components/AssignationDialog.vue
@@ -38,7 +38,7 @@ import {
 
 export default {
   name: "AssignationDialog",
-  props: ["isOpen", "selectedIssueId"],
+  props: ["isOpen", "selectedIssue"],
   data() {
     return {
       selectedTechnician: null,
@@ -65,7 +65,7 @@ export default {
         return this.isOpen;
       },
       set(value) {
-        this.$emit("updateIsOpenAssignation", value);
+        this.$emit("issueUpdated", value);
       },
     },
   },
