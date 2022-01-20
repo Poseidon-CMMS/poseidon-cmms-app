be680e7f4797a837cd5817482f14005e454ccc30
Fernando Navarro <f_e_r08@hotmail.com>
2022-01-20 01:16:51 -0300

    Adds leadings zeros to date formatter

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,6 +1,6 @@
 export const dateFormatter = (dateISOString, showTime = true) => {
     const dateObject = new Date(dateISOString);
-    const dateString = `${dateObject.getFullYear()}/${ dateObject.getMonth() + 1}/${dateObject.getDate()}`;
+    const dateString = `${dateObject.getFullYear()}/${('0' + (dateObject.getMonth()+1)).slice(-2)}/${('0' + dateObject.getDate()).slice(-2)}`;
     const timeString = showTime? ` ${dateObject.getHours()}:${dateObject.getMinutes()}`:""
     return dateString+timeString;
 }
