481f2b86cfb65f81f170d97c55de25c3c35b9032
Mauricio Lima <limamauricio303@gmail.com>
2022-01-31 00:47:39 -0300

    Switches to localstorage

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,13 +3,13 @@ import { createStore } from 'vuex'// Create a new store instance.
 export const store = createStore({
   state () {
     return {
-      user: JSON.parse(sessionStorage.getItem('user')) || null
+      user: JSON.parse(localStorage.getItem('user')) || null
     }
   },
   mutations: {
     setUser (state, user) {
       state.user = user;
-      sessionStorage.setItem('user',JSON.stringify(user));
+      localStorage.setItem('user',JSON.stringify(user));
     }
   }
 })
