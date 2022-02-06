2aa0357ef1e4cf7de40209b117b2ad85d86a1642
Mauricio Lima <limamauricio303@gmail.com>
2022-02-05 23:45:57 -0300

    Dashboard height fix

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="grid mt-3">
-    <div class="col-12 flex flex-row">
+  <div class="block mt-3">
+    <div>
       <iframe
         width="100%"
-        height="500px"
+        height="100%"
         src="https://datastudio.google.com/embed/reporting/1b085c28-6748-4c3a-b6ea-79f0528d1610/page/wGThC"
         frameborder="0"
-        style="border: 0"
+        style="border: 0; height: 75vh"
         allowfullscreen
       ></iframe>
     </div>
