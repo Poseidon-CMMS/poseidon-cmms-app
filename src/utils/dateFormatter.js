d0005b3b160635f6d3e8e5b804b8bd8aa0983a21
Mauricio Lima <limamauricio303@gmail.com>
2022-03-24 16:05:23 -0300

    Fixes date formatting

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,6 +1,7 @@
+import moment from 'moment';
+
 export const dateFormatter = (dateISOString, showTime = true) => {
-    const dateObject = new Date(dateISOString);
-    const dateString = `${dateObject.getFullYear()}/${('0' + (dateObject.getMonth()+1)).slice(-2)}/${('0' + dateObject.getDate()).slice(-2)}`;
-    const timeString = showTime? ` ${dateObject.getHours()}:${dateObject.getMinutes()}`:""
-    return dateString+timeString;
+    if(showTime)
+        return moment.format(dateISOString, 'DD-MM-YYYY HH:mm:ss');
+    else return moment.format(dateISOString, 'DD-MM-YYYY');
 }
