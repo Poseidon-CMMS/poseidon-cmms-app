3fdfb826d9cc11cc6cf758bef408e5010d4e571d
Mauricio Lima <limamauricio303@gmail.com>
2022-05-01 23:36:05 -0300

    Fixes routing and updates packages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Irrigators from "../views/Irrigators.vue";
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
@@ -64,7 +64,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
