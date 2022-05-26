46f15dcd8dace22754c5fe05e0afb0c28c161fa0
Mauricio Lima <limamauricio303@gmail.com>
2022-05-26 00:29:55 -0300

    Versioning update

--- src/api/backendUrl.js
+++ src/api/backendUrl.js
@@ -1,7 +1,7 @@
 let backendURL = 'SERVER_URL_SED_WILDCARD';
 
 const isLocalDeployment = backendURL.includes('SED_WILDCARD'); // if the backendURL has not been edited by sed
-console.log(`idd: ${isDockerDeployment} ${backendURL}`);
+console.log(`idd: ${isDockerDeployment} ${backendURL} V9`);
 if(isLocalDeployment) backendURL = process.env.VUE_APP_SERVER_URL;
 
 export {backendURL}
