5c15e58593da640f2a23127f9efbf286e770bbf4
Mauricio Lima <limamauricio303@gmail.com>
2022-05-26 00:17:44 -0300

    Adds logging

--- src/api/backendUrl.js
+++ src/api/backendUrl.js
@@ -1,6 +1,7 @@
 let backendURL = 'SERVER_URL_SED_WILDCARD';
 
 const isDockerDeployment = backendURL === ('SERVER_URL_'+'SED_WILDCARD')
+console.log(`idd: ${isDockerDeployment} ${backendURL}`)
 if(!isDockerDeployment) backendURL = process.env.VUE_APP_SERVER_URL;
 
 export {backendURL}
