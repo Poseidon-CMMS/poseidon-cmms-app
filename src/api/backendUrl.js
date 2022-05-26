7047e2fac54cc3eaaf6f67684423afe92adfd801
Mauricio Lima <limamauricio303@gmail.com>
2022-05-25 23:34:15 -0300

    Fixes local env variable loading

--- src/api/backendUrl.js
+++ src/api/backendUrl.js
@@ -1,6 +1,6 @@
 let backendURL = 'SERVER_URL_SED_WILDCARD';
 
-const isDockerDeployment = backendURL === ('SERVER_URL_'+'SED_WILDCARD')
+const isDockerDeployment = backendURL !== ('SERVER_URL_'+'SED_WILDCARD')
 if(!isDockerDeployment) backendURL = process.env.VUE_APP_SERVER_URL;
 
 export {backendURL}
