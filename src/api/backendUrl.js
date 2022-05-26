223abb5bf1eae4e81242ad10da46721355899475
Mauricio Lima <limamauricio303@gmail.com>
2022-05-26 00:29:24 -0300

    Backendurl logic update

--- src/api/backendUrl.js
+++ src/api/backendUrl.js
@@ -1,7 +1,7 @@
 let backendURL = 'SERVER_URL_SED_WILDCARD';
 
-const isDockerDeployment = backendURL === ('SERVER_URL_'+'SED_WILDCARD')
-console.log(`idd: ${isDockerDeployment} ${backendURL}`)
-if(!isDockerDeployment) backendURL = process.env.VUE_APP_SERVER_URL;
+const isLocalDeployment = backendURL.includes('SED_WILDCARD'); // if the backendURL has not been edited by sed
+console.log(`idd: ${isDockerDeployment} ${backendURL}`);
+if(isLocalDeployment) backendURL = process.env.VUE_APP_SERVER_URL;
 
 export {backendURL}
