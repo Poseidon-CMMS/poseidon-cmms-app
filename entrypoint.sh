fabedaebe877fcfa3942c38771d9c46a51fb6489
Mauricio Lima <limamauricio303@gmail.com>
2022-05-25 23:02:53 -0300

    Adds logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "Replacing env constants in JS"
 for file in $ROOT_DIR/js/*.js*;
 do
   echo "Processing $file ...";
+  echo "Server URL env param is: ${VUE_APP_SERVER_URL} / "${VUE_APP_SERVER_URL}
 
   sed -i 's|SERVER_URL_SED_WILDCARD|'${VUE_APP_SERVER_URL}'|g' $file
 
